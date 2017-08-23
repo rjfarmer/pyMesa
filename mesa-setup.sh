77238b2cb8f8ec4feeca809b9c3841131d7f0d4b
Robert Farmer <rjfarmer@asu.edu>
2017-08-23 10:58:14 +0100

    License files

--- mesa-setup.sh
+++ mesa-setup.sh
@@ -1,10 +1,22 @@
 #!/bin/bash
 
 # Tool to build mesa with python support
-# Licensed under GPLv2+
-# Author Robert Farmer
-# rjfarmer@asu.edu
-# 17/09/2017
+#Copyright (C) 2017  Robert Farmer <rjfarmer@asu.edu>
+
+#This file is part of pyMesa.
+
+#pyMesa is free software: you can redistribute it and/or modify
+#it under the terms of the GNU General Public License as published by
+#the Free Software Foundation, either version 2 of the License, or
+#(at your option) any later version.
+
+#pyMesa is distributed in the hope that it will be useful,
+#but WITHOUT ANY WARRANTY; without even the implied warranty of
+#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#GNU General Public License for more details.
+
+#You should have received a copy of the GNU General Public License
+#along with pyMesa. If not, see <http://www.gnu.org/licenses/>.
 
 
 if [ -z "$MESA_DIR" ];then
--- pyMesaUtils.py
+++ pyMesaUtils.py
@@ -1,3 +1,23 @@
+#!/bin/bash
+
+# Tool to build mesa with python support
+#Copyright (C) 2017  Robert Farmer <rjfarmer@asu.edu>
+
+#This file is part of pyMesa.
+
+#pyMesa is free software: you can redistribute it and/or modify
+#it under the terms of the GNU General Public License as published by
+#the Free Software Foundation, either version 2 of the License, or
+#(at your option) any later version.
+
+#pyMesa is distributed in the hope that it will be useful,
+#but WITHOUT ANY WARRANTY; without even the implied warranty of
+#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#GNU General Public License for more details.
+
+#You should have received a copy of the GNU General Public License
+#along with pyMesa. If not, see <http://www.gnu.org/licenses/>.
+
 from __future__ import print_function
 import gfort2py as gf
 import numpy as np
