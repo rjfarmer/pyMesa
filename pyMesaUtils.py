75521d9219b782b369f7d3dbfab59cba4144e0ae
Robert Farmer <r.j.farmer@uva.nl>
2018-02-01 17:33:53 +0100

    Add better handling of environment variables

--- pyMesaUtils.py
+++ pyMesaUtils.py
@@ -22,17 +22,22 @@ import gfort2py as gf
 import numpy as np
 import os
 import sys
+import subprocess
 
 if "MESA_DIR" not in os.environ:
     raise ValueError("Must set MESA_DIR environment variable")
+else:
+    MESA_DIR = os.environ.get('MESA_DIR')    
 
-MESA_DIR = os.environ.get('MESA_DIR')
-
-
-
+    
 DATA_DIR = os.path.join(MESA_DIR,'data')
 LIB_DIR = os.path.join(MESA_DIR,'lib')
 INCLUDE_DIR = os.path.join(MESA_DIR,'include')
+    
+if "LD_LIBRARY_PATH" not in os.environ:
+    raise ValueError("Must set LD_LIBRARY_PATH environment variable")
+elif LIB_DIR not in os.environ['LD_LIBRARY_PATH']:
+    raise ValueError("Must have $MESA_DIR/lib in LD_LIBRARY_PATH environment variable")
 
 
 RATES_DATA=os.path.join(DATA_DIR,'rates_data')
@@ -56,9 +61,7 @@ with open(os.path.join(DATA_DIR,'version_number'),'r') as f:
 
 
 def loadMod(module):
-    LIB_DIR = os.path.join(MESA_DIR,'lib')
-    INCLUDE_DIR = os.path.join(MESA_DIR,'include')
-
+    
     if module =='crlibm':
         SHARED_LIB = os.path.join(LIB_DIR,"libf2crlibm.so")
     else:
@@ -73,11 +76,43 @@ def loadMod(module):
         x = gf.fFort(SHARED_LIB,MODULE_LIB,rerun=True)
     except FileNotFoundError:
         pass
-        
+
     try:
         y = gf.fFort(SHARED_LIB,MODULE_DEF,rerun=True)
     except FileNotFoundError:
         pass
-    
+
     return x, y
+
+
+def buildModule(module):
+    cwd = os.getcwd()
+    os.chdir(os.path.join(MESA_DIR,module))
+    
+    try:
+        x=subprocess.call("./clean >/dev/null 2>/dev/null",shell=True)
+        if x:
+            raise ValueError("clean failed")
+        x = subprocess.call("./build_and_test >/dev/null 2>/dev/null",shell=True)
+        if x:
+            raise ValueError("Build_and_test failed")
+        x = subprocess.call("./export >/dev/null 2>/dev/null",shell=True)
+        if x:
+            raise ValueError("export failed")
+    except:
+        raise
+    finally:
+        os.chdir(cwd)
+        
+    os.chdir(LIB_DIR)
+    try:
+        x = subprocess.call("chrpath -r lib"+module+".so",shell=True)
+        if x:
+            raise ValueError("chrpath failed")
+    except:
+        raise
+    finally:
+        os.chdir(cwd)
+
+    print("Built "+str(module))
 
