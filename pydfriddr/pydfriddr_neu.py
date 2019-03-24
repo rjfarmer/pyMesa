a88a3df37e28d012a5cc803d15f76f0aafffe070
Robert Farmer <r.j.farmer@uva.nl>
2019-03-24 22:21:34 +0100

    Adjust neu deriv

--- pydfriddr/pydfriddr_neu.py
+++ pydfriddr/pydfriddr_neu.py
@@ -15,30 +15,27 @@ import matplotlib.pyplot as plt
 import sys
 
 
-def eval_x(x,*args,**kwargs):
+def eval_x(**kwargs):
     """
     This should take the x value being tested and return y(x)
     
     """
-    test_var=args[0]
+    test_var=kwargs['test_var']
     deriv=kwargs['deriv']
 
     if test_var is 't':
         index=neu_def.idneu_dT.get()-1
-        T=x
-        rho,abar,zbar=args[1:4]
-    elif test_var is 'r':
+    elif test_var is 'rho':
         index=neu_def.idneu_dRho.get()-1
-        rho=x
-        T,abar,zbar=args[1:4]
     elif test_var is 'a':
         index=neu_def.idneu_dabar.get()-1
-        abar=x
-        T,rho,zbar=args[1:4]
     elif test_var is 'z':
         index=neu_def.idneu_dzbar.get()-1
-        zbar=x
-        T,rho,abar=args[1:4]
+      
+    T=kwargs['t']
+    rho=kwargs['rho']  
+    abar = kwargs['a']
+    zbar = kwargs['z']
 
     log10_T=crlibm_lib.log10_cr(T)
     log10_Rho=crlibm_lib.log10_cr(rho)
@@ -47,13 +44,8 @@ def eval_x(x,*args,**kwargs):
     log10_Tlim=7.5
     flags=np.zeros(neu_def.num_neu_types.get())
     #flags[:]=True
-    flags[:]=False
-    if args[4] >= 0:
-        flags[args[4]]=True
-    else:
-        # flags[3]=True
-        flags[:]=True
-        # flags[-1]=False
+    flags[:]=True
+    #flags[-1]=False
     info=0
     
     num_neu_rvs=neu_def.num_neu_rvs.get()
@@ -61,25 +53,23 @@ def eval_x(x,*args,**kwargs):
     loss=np.zeros(num_neu_rvs)
     sources=np.zeros((num_neu_types,num_neu_rvs))
     
-    mass= 10**4
-    
-    res = neu_lib.neu_get(T, log10_T, rho, log10_Rho, abar, zbar, z2bar, log10_Tlim, flags, loss, sources,mass, info)
+    res = neu_lib.neu_get(T, log10_T, rho, log10_Rho, abar, zbar, z2bar, log10_Tlim, flags, loss, sources, info)
     if not deriv:
         index=neu_def.ineu.get()-1
         
     return res['loss'][index]
 
 
-def eval_dx(x,*args,**kwargs):
+def eval_dx(**kwargs):
     """
     This should take the x value being tested and return dy/dx at x
     
     """
-    return eval_x(x,*args,deriv=True)
+    return eval_x(**kwargs,deriv=True)
 
 
 
-def eval_func(start_x,start_step,func=eval_x,func_dx=eval_dx,verbose=True,*args):
+def eval_func(func=eval_x,func_dx=eval_dx,verbose=True,**kwargs):
     """
     Evaluate a function and its derivative by using ridders method of polynomial extrapolation
     Coded taken from $MESA_DIR/star/private/star_newton.f90 dfridr function
@@ -104,12 +94,24 @@ def eval_func(start_x,start_step,func=eval_x,func_dx=eval_dx,verbose=True,*args)
     safe=2.0
     dfridr=0.0
     a=np.zeros((ntab,ntab))
-    step=start_step
-    a[1,1] = (func(start_x+step,*args,deriv=False) - func(start_x-step,*args,deriv=False)) / (2.0*step) 
-    if verbose: print('\tdfdx',1,a[1,1],start_step)
+    step=kwargs['start_step']
+    start_x=kwargs['start_x']
+    
+    arg=kwargs['arg']
+    kwargs[arg]=start_x+step
+    a[1,1] = func(deriv=False,**kwargs) 
+    
+    kwargs[arg]=start_x-step
+    a[1,1] = (a[1,1]-func(deriv=False,**kwargs)) / (2.0*step) 
+    if verbose: print('\tdfdx',1,a[1,1],step)
     for i in range(2,ntab):
         step=step/con
-        a[1,i]=(func(start_x+step,*args,deriv=False) - func(start_x-step,*args,deriv=False)) / (2.0*step)
+        
+        kwargs[arg]=start_x+step
+        a[1,i]=func(deriv=False,**kwargs)
+        
+        kwargs[arg]=start_x-step
+        a[1,i]= (a[1,i]- func(deriv=False,**kwargs)) / (2.0*step)
         if verbose: print('\tdfdx',i,a[1,i],step)
         fac=con2
         for j in range(2,i+1):
@@ -124,7 +126,8 @@ def eval_func(start_x,start_step,func=eval_x,func_dx=eval_dx,verbose=True,*args)
             if verbose: print("Higher order is worse")
             break
         
-    dvardx_0 = func_dx(start_x,*args)
+    kwargs[arg]=start_x
+    dvardx_0 = func_dx(**kwargs)
     xdum=(dfridr-dvardx_0)/(np.maximum(np.abs(dvardx_0),10**-50))
     if verbose: print()
     if verbose: print('analytic numeric err rel_diff',dvardx_0,dfridr,err,xdum)
@@ -132,92 +135,76 @@ def eval_func(start_x,start_step,func=eval_x,func_dx=eval_dx,verbose=True,*args)
     return dvardx_0,dfridr,err,xdum
 
 
-def bestfit(x,*args):
+def bestfit(**kwargs):
+    kwargs.pop('deriv',None)
     tmp=[]
     err=[]
-    tol=10**-3
-    for j in range(1,10):
-        start_step=x/(10.0**j)
-        r=eval_func(x,start_step,eval_x,eval_dx,False,*args)
+    for j in range(0,15):
+        kwargs['start_step']=kwargs[kwargs['arg']]/(10.0**j)
+        kwargs['start_x']=kwargs[kwargs['arg']]
+        r=eval_func(eval_x,eval_dx,verbose=False,**kwargs)
         tmp.append(r)
         try:
             err.append(np.abs(r[2]/r[1]))
         except ZeroDivisionError:
             err.append(np.nan)
-       
+        
     try:
         best_fit=np.nanargmin(err)
     except:
         best_fit=-1
         
-    if best_fit>=0 and err[best_fit] < tol:
+    if best_fit>=0 and err[best_fit] < kwargs['tol']:
         r=tmp[best_fit]
     else:
-        print("Failed for "+str(x))
+        print("Failed for "+str(kwargs[kwargs['arg']]))
     
     return r
-
-def driver(xmin,xmax,num_steps,log=False,tol=10**-6,*args):
-
-    if log:
-        xvalues=np.linspace(np.log10(xmin),np.log10(xmax),num_steps)
-        xvalues=10**xvalues
+    
+def plot2d(xmin,xmax,ymin,ymax,xsteps,ysteps,name,title='',**kwargs):    
+    
+    if kwargs['log']:
+        xvalues=np.linspace(np.log10(xmin),np.log10(xmax),int(xsteps))
+        yvalues=np.linspace(np.log10(ymin),np.log10(ymax),int(ysteps))
     else:
-        xvalues=np.linspace(xmin,xmax,num_steps)
-
-    res=[]
-    for i in xvalues:
-        res.append(bestfit(i,args))
+        xvalues=np.log10(np.linspace(xmin,xmax,int(xsteps)))
+        yvalues=np.log10(np.linspace(ymin,ymax,int(ysteps)))
     
-    return res
-    
-def plot1d(xmin,xmax,xsteps):    
-    xvalues=np.linspace(np.log10(xmin),np.log10(xmax),xsteps)
-        
-    z=[]
-    for i in xvalues:
-       # print(i,j)
-        r=bestfit(10**i,0,10**9.0,56.0,26.0)
-        z[-1].append(np.log10(np.abs(r[3])))
-    
-    plt.ylabel('log abs dfriddr err')
-    plt.plot(x,z)
-    plt.scatter(x,z)
-    plt.show()
-    
-    
-def plot2d(xmin,xmax,ymin,ymax,xsteps,ysteps,deriv_flag,neu_flag,name,rev=False,title=''):    
-    
-    xvalues=np.linspace(np.log10(xmin),np.log10(xmax),int(xsteps))
-    yvalues=np.linspace(np.log10(ymin),np.log10(ymax),int(ysteps))
-    
-        
+    kwargs['tol']=10**-3
     z=[]
     
     for i in xvalues:
+        print(i)
         z.append([])
         for j in yvalues:
-            #print(i,j)
-            r=bestfit(10**i,deriv_flag,10**j,56.0,26.0,neu_flag)
+            kwargs[kwargs['arg']]=10**i
+            kwargs[kwargs['arg2']]=10**j
+            r=bestfit(**kwargs)
             z[-1].append(np.log10(np.abs(r[3])))
     
     z=np.array(z)
     fig=plt.figure()
     ax=fig.add_subplot(111)
-    
-    extent=(np.log10(xmin),np.log10(xmax),np.log10(ymin),np.log10(ymax))
-    if rev:
+    extent=(xvalues.min(),xvalues.max(),yvalues.min(),yvalues.max())
+    if kwargs['rev']:
         z=z.T
-        extent=(np.log10(ymin),np.log10(ymax),np.log10(xmin),np.log10(xmax))
+        extent=(yvalues.min(),yvalues.max(),xvalues.min(),xvalues.max())
     
-    cax=ax.imshow(z.T,extent=extent,aspect='auto',vmin=np.maximum(np.min(z),-16.0),vmax=0.0,origin='lower')
+    cax=ax.imshow(z.T,extent=extent,aspect='auto',vmin=-16.0,vmax=0.0,origin='lower')
     cb=fig.colorbar(cax)
     
     cb.set_label('log abs dfriddr err')
     ax.set_title(title)
-    ax.set_xlabel('logT')
-    ax.set_ylabel('LogRho')
+    
+    if  kwargs['rev']:
+        ax.set_xlabel('log '+kwargs['arg2'])
+        ax.set_ylabel('log '+kwargs['arg'])
+    else:
+        ax.set_xlabel('log '+kwargs['arg'])
+        ax.set_ylabel('log '+kwargs['arg2'])
+    
     fig.savefig(name)
+    # plt.show()
     plt.close(fig)
     
 def plot2dNeu(xmin,xmax,ymin,ymax,xsteps,ysteps,name):    
@@ -268,33 +255,26 @@ crlibm_lib.crlibm_init()
 const_lib.const_init(pym.MESA_DIR,ierr)
 
 neu_lib,neu_def = pym.loadMod(mod)
-#print(eval_x(10**9.0,'r',10**10.1,56,26,3,deriv=False))
-
-#print(eval_func(10**9.0,10**0.0,eval_x,eval_dx,False,'t',10**2.0,56,26,4))
 
 prefix=sys.argv[1]
 
 
-steps=100
+steps=200
 
 tmin=10**6.9
 tmax=10**10.0
 rmin=10**-1
 rmax=10**10
 
-j='t'
-plot2d(tmin,tmax,rmin,rmax,steps,steps,j,-1,prefix+'_'+j+'_all.pdf',title='dt')
-# for i in range(0,5):
-    # print(j,i)
-    # plot2d(tmin,tmax,rmin,rmax,steps,steps,j,i,prefix+'_'+j+'_'+str(i)+'.pdf',title='dt')
+extraargs={'a':12.0,'z':12.0}
 
+plot2d(tmin,tmax,rmin,rmax,steps,steps,prefix+'_dt.pdf',title='dt',
+		test_var='t',arg='t',arg2='rho',log=True,rev=True,**extraargs)
 
-j='r'
-plot2d(rmin,rmax,tmin,tmax,steps,steps,j,-1,prefix+'_'+j+'_all.pdf',rev=True,title='drho')
-# for i in range(0,5):
-    # print(j,i)
-    # plot2d(rmin,rmax,tmin,tmax,steps,steps,j,i,prefix+'_'+j+'_'+str(i)+'.pdf',rev=True,title='drho')
+plot2d(rmin,rmax,tmin,tmax,steps,steps,prefix+'_drho.pdf',title='drho',
+		test_var='rho',arg='rho',arg2='t',log=True,rev=False,**extraargs)
+		
 
-plot2dNeu(tmin,tmax,rmin,rmax,steps,steps,prefix+'_neu.pdf')
+#plot2dNeu(tmin,tmax,rmin,rmax,steps,steps,prefix+'_neu.pdf')
 
 
