d7361fea7e591c809de684927731bb7a23699d16
Robert Farmer <r.j.farmer@uva.nl>
2020-02-14 14:43:43 +0100

    Cleanup gyre

--- pymesa/gyre.py
+++ pymesa/gyre.py
@@ -1,10 +1,8 @@
 import pymesa.pyMesaUtils as pym
 
 class gyre(object):
-	def __init__(self, namelist):
-		self.gyre_lib, _ = pym.loadMod("gyre")
+    def __init__(self, namelist):
+        self.gyre_lib, _ = pym.loadMod("gyre")
         self.namelist = namelist
         self.gyre_lib.gyre_init(namelist)
 
-    def __del__(self):
-        self.gyre_lib.gyre_final()
