af0bd06ac7531cc1c40f011eca9086f81c8f1fd7
Robert Farmer <r.j.farmer@uva.nl>
2020-02-14 16:04:48 +0100

    Wrapper for neu code

--- pymesa/neu.py
+++ pymesa/neu.py
@@ -1,7 +1,7 @@
 import pymesa.pyMesaUtils as pym
+import numpy as np
 
-
-class net(object):
+class neu(object):
     def __init__(self):
         self.const_lib, self.const_def = pym.loadMod("const")
         
@@ -10,20 +10,40 @@ class net(object):
         
         self.neu_lib,self.neu_def = pym.loadMod("neu")
 
+    def getNeu(self,T,Rho,abar,zbar,z2bar,log10_Tlim=7.5):
+        log10T = np.log10(T)
+        log10Rho = np.log10(Rho)
+        
+        flags=np.zeros(self.neu_def.num_neu_types)
+        flags[:]=True
+        loss=np.zeros(self.neu_def.num_neu_rvs)
+        sources=np.zeros((self.neu_def.num_neu_types,self.neu_def.num_neu_rvs))
 
-
-# T=10**9.0
-# log10_T=np.log10(T)
-# Rho=10**9.0
-# log10_Rho=np.log10(T)
-# abar=1.0
-# zbar=1.0
-# z2bar=1.0
-# log10_Tlim=7.5
-# flags=np.zeros(neu_def.num_neu_types.get())
-# flags[:]=True
-# loss=np.zeros(neu_def.num_neu_rvs.get())
-# sources=np.zeros((neu_def.num_neu_types.get(),neu_def.num_neu_rvs.get()))
-# info=0
-
-# res = neu_lib.neu_get(T, log10_T, Rho, log10_Rho, abar, zbar, z2bar, log10_Tlim, flags, loss, sources, info)
+        res = self.neu_lib.neu_get(T, log10T, Rho, log10Rho, abar, zbar, z2bar, log10_Tlim, flags, loss, sources, 0)
+        
+        l = res['loss']
+        s = res['sources']
+        
+        neu_types = {'pair_neu_type':self.neu_def.pair_neu_type-1,
+                    'plas_neu_type':self.neu_def.plas_neu_type-1,
+                    'phot_neu_type':self.neu_def.phot_neu_type-1,
+                    'brem_neu_type':self.neu_def.brem_neu_type-1,
+                    'reco_neu_type':self.neu_def.reco_neu_type-1
+        }
+                    
+        neu_rvs = {'ineu':self.neu_def.ineu-1,
+                    'idneu_dT':self.neu_def.idneu_dT-1,
+                    'idneu_dRho':self.neu_def.idneu_dRho-1,
+                    'idneu_dabar':self.neu_def.idneu_dabar-1,
+                    'idneu_dzbar':self.neu_def.idneu_dzbar-1
+        }
+        
+        loss = {}
+        sources = {}
+        for k_t, v_t in neu_types.items():
+            loss[k_t] = l[v_t]
+            sources[k_t] = {}
+            for k_s, v_s in neu_rvs.items():
+                sources[k_t][k_s] = s[v_t,v_s]
+    
+        return loss, sources
