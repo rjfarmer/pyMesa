5c9a5eff1a7dec45343cc2804990434dbc9960fb
Robert Farmer <r.j.farmer@uva.nl>
2020-03-14 22:15:13 +0100

    Finish installer

--- pymesa/pyMesaUtils.py
+++ pymesa/pyMesaUtils.py
@@ -28,6 +28,9 @@ import urllib
 import hashlib
 import zipfile
 import tarfile
+import io
+import svn.remote
+import pathlib
 
 MIN_VERSION = 12780
 
@@ -48,7 +51,7 @@ class mesa(object):
     def _get_var(self,var,name):
         if var is None:
             if name in os.environ:
-                return os.environ.get('MESA_DIR')
+                return os.environ.get(name)
             else:
                 raise ValueError("Must set "+str(name)+" environment variable or pass as an argument")
         else:
@@ -99,6 +102,19 @@ class mesa(object):
         # Check we compiled with shared libraries
         if not os.path.exists(os.path.join(self.LIB_DIR,'libstar.' + self.LIB_EXT)):
             raise ValueError("Recompile MESA with USED_SHARED = yes")
+            
+    def build_shared(self):
+        filename = os.path.join(self.MESA_DIR,'utils','makefile_header')
+        
+        with open(filename,'r') as f:
+            lines = f.readlines()
+            
+        for idx,i in enumerate(lines):
+            if 'USE_SHARED =' in i:
+                lines[idx] = 'USE_SHARED = YES'
+                
+        with open(filename,'w') as f:
+            f.writelines(lines)
     
     def loadMod(module):
         MODULE_LIB = os.path.join(self.INCLUDE_DIR,module+"_lib.mod")
@@ -130,33 +146,30 @@ class mesa(object):
         
         
     def clean(self,folder):
-        self._run_cmd(folder,'./clean')
+        print(folder)
+        self._runCmd('./clean',folder)
  
     def mk(self,folder):
-        self._run_cmd(folder,'./mk')           
+        self._runCmd('./mk',folder)
     
     def rn(self,folder):
-        self._run_cmd(folder,'./rn')
+        self._runCmd('./rn',folder)
         
     def re(self,folder,photo):
-        self._run_cmd(folder,'./re ' + str(photo))
+        self._runCmd('./re',folder,str(photo))
         
     def build_and_test(self,folder):
-        self._run_cmd(folder,'./build_and_test')
+        self._runCmd('./build_and_text',folder)
         
     def export(self,folder):
-        self._run_cmd(folder,'./export')
+        self._runCmd(folder,'./export',self._sdk_path)
+        
+    def _runCmd(self,cmd,folder,args=''):
+        if self._sdk_path is not None:
+            subprocess.Popen(['source '+self._sdk_path+'; '+cmd+' '+args],shell=True,cwd=folder,executable='/bin/bash')
+        else:
+            subprocess.Popen([cmd + ' ' + args],shell=True,cwd=folder,executable='/bin/bash')
     
-    def _run_cmd(self,folder,cmd):
-        cwd = os.getcwd()
-        os.chdir(folder)    
-        try:
-            x=subprocess.call(str(cmd)+ " >/dev/null 2>/dev/null",shell=True)
-        except:
-            raise
-        finally:
-            os.chdir(cwd)
-
     def buildModule(self,module):
         self.clean(module)
         self.build_and_test(module)
@@ -273,58 +286,43 @@ class mesa(object):
         
 
     def _runcrpath(self,filename):
-        cwd = os.getcwd()
-        os.chdir(self.LIB_DIR)
-        try:
-            x = subprocess.call("chrpath -r " + str(filename),shell=True, stdout=open(os.devnull, 'wb'))
-            if x:
-                raise ValueError("chrpath failed on " + str(filename))
-        except:
-            raise
-        finally:
-            os.chdir(cwd)      
+        if subprocess.call(["chrpath","-r",str(filename)],shell=True, stdout=open(os.devnull, 'wb'),cwd=self.LIB_DIR):
+            raise ValueError("chrpath failed on " + str(filename))  
             
-    def _isTcsh(self):
-        shell = os.environ['SHELL']
-        if 'tcsh' in shell:
-            return True
-        else:
-            return False
-            
-    def _isBash(self):
-        shell = os.environ['SHELL']
-        if 'bash' in shell:
-            return True
-        else:
-            return False
-            
-            
+
     def setNonSDK(self):
+        self.nonsdk = True
+        self._sdk_path = None
         shutil.copy2(os.path.join(self.MESA_DIR,'utils','makefile_header_nonsdk'),os.path.join(self.MESA_DIR,'utils','makefile_header'))
         
     def activateSDK(self):
+        self.MESASDK_ROOT = os.path.realpath(self.MESASDK_ROOT)
+        os.environ['MESASDK_ROOT'] = self.MESASDK_ROOT
         if self.MESASDK_ROOT is not None:
-            if self.isTcsh():
-                self._run_cmd('./','source '+os.path.join(self.MESASDK_ROOT,'bin','mesasdk_init.csh'))
-            else:
-                self._run_cmd('./','source '+os.path.join(self.MESASDK_ROOT,'bin','mesasdk_init.sh'))
+            self._sdk_path = os.path.join(self.MESASDK_ROOT,'bin','mesasdk_init.sh')
         else:
             raise ValueError("Must set MESASDK_ROOT first")
+            
+        subprocess.Popen('source '+self._sdk_path, shell=True, executable="/bin/bash")
+
+        
                         
     def downloadZip(self,version=-1):
-        data = self._getMesadata()
+        data = self._getMesaData()
         if version == -1:
             version = self.getLatestVersion()
         
-        return self._download(data[version])
+        return self._download(data[version],self.MESA_DIR)
         
         
-    def downloadSVN(self,version=-1):
+    def downloadSVN(self,version=None):
         mesa_svn = 'https://subversion.assembla.com/svn/mesa%5Emesa/trunk'
         if version == -1:
-            versions = self.getLatestVersion()
+            version = self.getLatestVersion()
             
-        self._run_cmd('./','svn co -r '+str(version) + ' ' + mesa_svn + ' ' + self.MESA_DIR)
+        print("Checking out "+ str(version))
+        r = svn.remote.RemoteClient(mesa_svn)
+        r.checkout(self.MESA_DIR,version)
         
         return self.MESA_DIR
         
@@ -336,9 +334,9 @@ class mesa(object):
         if version == -1:
             version = self.getLatestSDKVersion()
         
-        return self._download(data[version])
+        return self._download(data[version],self.MESASDK_ROOT)
         
-    def _download(self,data):
+    def _download(self,data,filename):
 
         url = data['url']
         fhash = data['hash']
@@ -346,7 +344,7 @@ class mesa(object):
             
         fileout = urllib.parse.urlparse(url)
         file_out = os.path.basename(fileout.path)
-        output = os.path.join(os.path.basename(self.MESASDK_ROOT),file_out)
+        output = filename+'.'+extension
         self._downloadFile(output,url,fhash)
             
         print("Extracting file")
@@ -358,6 +356,9 @@ class mesa(object):
     def preinstallSDK(self):
         sdk_path = self.downloadSDK()
         
+        self.MESASDK_ROOT = os.realpath(sdk_path)
+        os.environ['MESASDK_ROOT'] = os.realpath(sdk_path)
+        
     def installSDK(self):
         self.activateSDK()
         
@@ -367,16 +368,20 @@ class mesa(object):
     def preinstallMesa(self,zip_src=True,version=-1):
         if zip_src:
             mesa_path = self.downloadZip(version=version)
-            self._extract(mesa_path,self.MESA_DIR)
         else:
-            os.chdir(self.MESA_DIR)
-            self.downloadSVN(version=version)
+            os.chdir(os.path.basename(self.MESA_DIR))
+            mesa_path = self.downloadSVN(version=version)
+        
+        self.MESA_DIR = os.path.realpath(mesa_path)
+        os.environ['MESA_DIR'] = os.path.realpath(mesa_path)
             
-    def installMesa():
+            
+    def installMesa(self):
         self.clean(self.MESA_DIR)
+        self.build_shared()
         self.mk(self.MESA_DIR)
         
-    def postinstallMesa():
+    def postinstallMesa(self):
         pass
         
     def listMesaVersions(self):
@@ -451,21 +456,41 @@ class mesa(object):
         
     def _downloadFile(self,filename,url,fhash):
         print("Downloading ",url)
-        urllib.request.urlretrieve(url,filename)
+        
+        resp = urllib.request.urlopen(url)
+        length = resp.getheader('content-length')
+        blocksize = 1024
+        if length:
+            length = int(length)
+        else:
+            length = 100000
+
+        with open(filename,'wb') as f:
+            size = 0
+            while True:
+                buff = resp.read(blocksize)
+                if not buff:
+                    break
+                f.write(buff)
+                size += len(buff)
+                if length:
+                    print('Done {:.2f}%\r'.format((size/length)*100), end='')
+            print()
+                
         if not self._checkHash(filename, fhash):
             raise
         
         
     def _extractFile(self, file_in, fold_out):
-        if filename.endswith('.zip'):
+        if file_in.endswith('.zip'):
             extract_f = zipfile.ZipFile
-        elif filename.endswith('.tar.gz'):
-            extract_f = tarfile
+        elif file_in.endswith('.tar.gz'):
+            extract_f = tarfile.open
         else:
             raise ValueError("Filetype not supported")
             
-        with extract_f (file_in, 'r') as f:
-            f.extractallf(fold_out)
+        with extract_f(file_in, 'r') as f:
+            f.extractall(os.path.dirname(fold_out))
 
     def _checkHash(self, filename, fhash):
         if fhash.startswith('md5:'):
@@ -492,6 +517,9 @@ class mesa(object):
     def _getRootFolder(self):
         return os.path.abspath(os.path.dirname(__file__))
         
+        
+    def listTestCases(self):
+        return [p.name for p in pathlib.Path(os.path.join(self.MESA_DIR,'star','test_suite')).iterdir()]
 
     @property
     def defaults(self):
