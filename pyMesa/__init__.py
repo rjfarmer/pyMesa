a44647e7ab93ebb955c26ab78257b67c039ba6a7
Rob Farmer <robert.j.farmer37@gmail.com>
2026-06-20 18:20:54 +0100

    Make package importable when testing

--- pyMesa/__init__.py
+++ pyMesa/__init__.py
@@ -3,14 +3,17 @@ import os
 
 from .version import __version__
 
-if "MESA_DIR" not in os.environ:
-    raise ValueError("Must set MESA_DIR environment variable")
-else:
-    MESA_DIR = os.environ.get("MESA_DIR")
+if "GITHUB_TESTING" not in os.environ:
+    # Don't load MESA if we are testing on github actions
 
-if pathlib.Path(MESA_DIR).joinpath("build").exists():
-    # Post 2026 versions
-    from .pyMesaUtils_2026 import *
-else:
-    # Older MESA's pre 2026
-    from .pyMesaUtils_pre2026 import *
+    if "MESA_DIR" not in os.environ:
+        raise ValueError("Must set MESA_DIR environment variable")
+    else:
+        MESA_DIR = os.environ.get("MESA_DIR")
+
+    if pathlib.Path(MESA_DIR).joinpath("build").exists():
+        # Post 2026 versions
+        from .pyMesaUtils_2026 import *
+    else:
+        # Older MESA's pre 2026
+        from .pyMesaUtils_pre2026 import *
